9fbbae1986a43ec7c0f50e0b4b797fe0cb944196
e.chepelev <>
2023-10-31 22:24:06 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .executable(name: "proxy-server-cli", targets: ["ProxyServerCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", .exactItem(.init(stringLiteral: "2.51.0"))),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
